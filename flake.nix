caf98d04eac44a4c4d6e889db319350bfda8a6ab
Kori <daniilignatev290@proton.me>
2026-05-19 02:24:42 +0300

    Create flake.nix for anime cursor collection
    Added a flake.nix file for anime cursor collection on NixOS.

--- flake.nix
+++ flake.nix
@@ -1,1 +1,24 @@
+{
+  description = "Коллекция аниме курсоров для NixOS";
 
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Поддерживаем стандартные x86_64 системы (можно расширить)
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      # 1. Экспортируем пакеты по отдельности (для nix build)
+      packages.${system} = import ./pkgs { inherit pkgs; };
+
+      # 2. Экспортируем наш NixOS-модуль
+      nixosModules.default = import ./modules {
+        # Передаем наши пакеты внутрь модуля, чтобы он их видел
+        cursorPackages = self.packages.${system};
+      };
+    };
+}
