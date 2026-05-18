f65fa298a45b90b8cdb268e1203a34e5d6750572
Kori <daniilignatev290@proton.me>
2026-05-19 02:27:18 +0300

    Create Nix derivation for Neon Genesis Evangelion
    Add Nix expression for Neon Genesis Evangelion package

--- pkgs/evangelion.nix
+++ pkgs/evangelion.nix
@@ -1,1 +1,20 @@
+{ stdenv, fetchurl }:
 
+stdenv.mkDerivation rec {
+  pname = "Neon-Genesis-Evangelion";
+  version = "1.0";
+
+  src = fetchurl {
+    url = "https://ссылка-на-прямое-скачивание-архива/eva-cursor.tar.gz";
+    sha256 = "0000000000000000000000000000000000000000000000000000000000000000"; # Гитхаб/Nix подскажет правильный хэш
+  };
+
+  dontBuild = true;
+  dontConfigure = true;
+
+  installPhase = ''
+    mkdir -p $out/share/icons/${pname}
+    # Распаковываем содержимое архива прямо в папку иконки
+    cp -r . $out/share/icons/${pname}/
+  '';
+}
