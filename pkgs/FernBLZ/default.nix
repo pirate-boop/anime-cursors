b48715a7013b4dc63594d96cd3babec71e13d38b
Kori <daniilignatev290@proton.me>
2026-05-19 03:15:43 +0300

    Create default.nix for FernBLZ cursor theme
    Add Nix derivation for FernBLZ cursor theme

--- pkgs/FernBLZ/default.nix
+++ pkgs/FernBLZ/default.nix
@@ -1,1 +1,22 @@
+{ stdenv }:
 
+stdenv.mkDerivation rec {
+  pname = "FernBLZ";
+  version = "1.0";
+
+  # Берём исходники из текущей папки
+  src = ./.;
+
+  dontBuild = true;
+  dontConfigure = true;
+
+  installPhase = ''
+    # Создаем системный путь для темы курсоров
+    mkdir -p $out/share/icons/${pname}
+
+    # Копируем строго нужные файлы и папки.
+    # Папку preview и файл default.nix сюда НЕ пишем, 
+    # чтобы они не засоряли системный раздел /nix/store
+    cp -r index.theme cursors $out/share/icons/${pname}/
+  '';
+}
